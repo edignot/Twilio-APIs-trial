23b520d9639c16868476ea0fc890d1ecd792c8f1
Ed Ignot <edignot@gmail.com>
2020-10-22 13:20:23 -0600

    Send text message with node

---
+++ send-sms.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const fromNumber = process.env.FROM_NUMBER
+const toNumber = process.env.TO_NUMBER
+
+const client = require('twilio')(accountSid, authToken)
+
+client.messages
+  .create({
+    to: toNumber,
+    from: fromNumber,
+    body: 'Hello Edita!!!',
+  })
+  .then((message) => console.log(message))
